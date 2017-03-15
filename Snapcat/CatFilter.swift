b037bf6edb1e0aa483a0ab67b3fc6d243bda7d84
Luke Brody <lukebrody@berkeley.edu>
2017-03-14 21:35:11 -0700

    Relatively working

--- Snapcat/CatFilter.swift
+++ Snapcat/CatFilter.swift
@@ -42,17 +42,15 @@ class CatFilter {
         case rightEye = "RightEye"
     }
     
-    let features : [Feature : CIImage]
+    let features : [Feature : UIImage]
     
     init(name: String) {
         
-        var features = [Feature : CIImage]()
+        var features = [Feature : UIImage]()
         
         //If any of these image loads fails, the entry in the dictionary is simply not set.
         for feature in [Feature.face, .mouth, .leftEye, .rightEye] {
-            if let image = UIImage(named: name + feature.rawValue) {
-                features[feature] = CIImage(image: image)
-            }
+            features[feature] =  UIImage(named: name + feature.rawValue)
         }
         
         self.features = features
@@ -92,131 +90,6 @@ class CatFilter {
         transformFilter.setValue(featureImage, forKey: "inputImage")
         
         return transformFilter.outputImage!
-    }
-    
-    func apply(toImage pixelBuffer: CVImageBuffer) -> UIImage {
-        
-        let image = CIImage(cvImageBuffer: pixelBuffer)
-        
-        /*
-         The image comes in landscape, and not flipped so it looks weird and un-mirror-like.
-         
-         We need to rotate the image 90º clockwise, and flip it.
-         
-         The comment below each transform operation shows the transformation state after said operation.
-         w = the original width of the image
-         h = the original height of the image
-        */
-        
-        var transform = CGAffineTransform.identity
-        
-        /*
-          _____ (w,h)
-         |     |
-         |_____|
-    (0,0)
-         
-         */
-        
-        transform = transform.translatedBy(x: image.extent.height, y: image.extent.width)
-         
-         /*
-              _____ (h+w,w+h)
-             |     |
-             |_____|
-           (h,w)
-         x
-     (0,0)
-        */
-        
-        transform = transform.rotated(by: -CGFloat(M_PI_2))
-        
-         /*
-                  ____(h+h,w)
-                 |    |
-                 |    |
-                 |____|
-            (h,0)
-         */
-        
-        //We're going to mirror the image so it looks like a ...mirror
-        transform = transform.scaledBy(x: 1, y: -1)
-        
-        /*
-          ____ (h,w)
-         |    |
-         |    |
-         |____|
-     (0,0)
-         */
-        
-        transformFilter.setValue(NSValue(cgAffineTransform: transform), forKey: "inputTransform")
-        
-        //first, rotate the image because it comes in landscape
-        transformFilter.setValue(image, forKey: "inputImage")
-        
-        let rotated = transformFilter.value(forKey: "outputImage") as! CIImage
-        
-        var withFeatures = rotated
-        
-        func compositeFeature(image: CIImage) {
-            compositeFilter.setValue(withFeatures, forKey: "inputBackgroundImage")
-            compositeFilter.setValue(image, forKey: "inputImage")
-            
-            withFeatures = compositeFilter.value(forKey: "outputImage") as! CIImage
-        }
-        
-        //in the rotate image, find features
-        for feature in faceRecognizer.features(in: rotated) {
-            switch feature.type {
-            case CIFeatureTypeFace:
-                
-                let faceFeature = feature as! CIFaceFeature
-                
-                //if we have a image for the face bounds, draw it
-                if let boundsImage = features[.face] {
-                    
-                    let transformed = self.transform(featureImage: boundsImage, toBounds: faceFeature.bounds, rotation: faceFeature.hasFaceAngle ? CGFloat(faceFeature.faceAngle) : nil)
-                    
-                    compositeFeature(image: transformed)
-                    
-                }
-                
-                //if we have a mouth image, draw it as well
-                if let mouthImage = features[.mouth], faceFeature.hasMouthPosition {
-                    
-                    let mouthBounds = faceFeature.bounds.centered(at: faceFeature.mouthPosition, scaledBy: CGSize(width: 0.5, height: 0.2))
-                    
-                    let transformed = self.transform(featureImage: mouthImage, toBounds: mouthBounds, rotation: faceFeature.hasFaceAngle ? CGFloat(faceFeature.faceAngle) : nil)
-                    
-                    compositeFeature(image: transformed)
-                    
-                }
-                
-                //if we have a mouth image, draw it as well
-                if let leftEyeImage = features[.leftEye], faceFeature.hasLeftEyePosition {
-                    
-                    let leftEyeBounds = faceFeature.bounds.centered(at: faceFeature.leftEyePosition, scaledBy: CGSize(width: 0.2, height: 0.2))
-                    
-                    let transformed = self.transform(featureImage: leftEyeImage, toBounds: leftEyeBounds, rotation: faceFeature.hasFaceAngle ? CGFloat(faceFeature.faceAngle) : nil)
-                    
-                    compositeFeature(image: transformed)
-                    
-                }
-                
-            default:
-                break
-            }
-        }
-        
-        //always crop the result to the original image size
-        let vector = CIVector(cgRect: rotated.extent)
-        cropFilter.setValue(vector, forKey: "inputRectangle")
-        cropFilter.setValue(withFeatures, forKey: "inputImage")
-        
-        let cropped = cropFilter.value(forKey: "outputImage") as! CIImage
-        
-        return UIImage(ciImage: cropped)
     }
 }
 
--- Snapcat/FaceViewController.swift
+++ Snapcat/FaceViewController.swift
@@ -9,10 +9,59 @@
 import UIKit
 import AVFoundation
 
+class FaceLayer : CALayer {
+    
+    override init() {
+        super.init()
+        
+        addSublayer(mouth)
+        addSublayer(leftEye)
+        addSublayer(rightEye)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(filter : CatFilter) {
+    
+        contents = filter.features[.face]?.cgImage
+        mouth.contents = filter.features[.mouth]?.cgImage
+        leftEye.contents = filter.features[.leftEye]?.cgImage
+        rightEye.contents = filter.features[.rightEye]?.cgImage
+    }
+    
+    let mouth = CALayer()
+    let leftEye = CALayer()
+    let rightEye = CALayer()
+    
+    func process(feature: CIFaceFeature) {
+        bounds = feature.bounds
+    }
+    
+}
+
 class FaceViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    var filter : CatFilter!
-    @IBOutlet weak var imageView: UIImageView!
+    var filter : CatFilter! {
+        didSet {
+            faceLayer.contents = filter.features[.face]?.cgImage
+            mouthLayer.contents = filter.features[.mouth]?.cgImage
+            leftEyeLayer.contents = filter.features[.leftEye]?.cgImage
+            rightEyeLayer.contents = filter.features[.rightEye]?.cgImage
+        }
+    }
+    
+    
+    let (context, detector) : (CIContext, CIDetector) = {
+        let context = CIContext()
+        let detector = CIDetector(ofType: CIDetectorTypeFace, context: context, options: [
+            CIDetectorTracking: true,
+            CIDetectorNumberOfAngles: 5
+            ])!
+        
+        return (context, detector)
+    }()
     
     //We need the output so we can set ourselves as delegate after
     lazy var session : AVCaptureSession = {
@@ -31,6 +80,21 @@ class FaceViewController: UIViewController, AVCaptureVideoDataOutputSampleBuffer
         return result
     }()
     
+    let faceLayer = CALayer()
+    let mouthLayer = CALayer()
+    let leftEyeLayer = CALayer()
+    let rightEyeLayer = CALayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.layer.addSublayer(faceLayer)
+        
+        faceLayer.addSublayer(mouthLayer)
+        faceLayer.addSublayer(leftEyeLayer)
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         session.startRunning()
     }
@@ -39,11 +103,145 @@ class FaceViewController: UIViewController, AVCaptureVideoDataOutputSampleBuffer
         session.stopRunning()
     }
     
+    private let transformFilter = CIFilter(name: "CIAffineTransform")!
+    
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
         
         let pixelBuffer : CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
         
-        imageView.image = filter.apply(toImage: pixelBuffer)
+        let image = CIImage(cvImageBuffer: pixelBuffer)
+        
+        /*
+         The image comes in landscape, and not flipped so it looks weird and un-mirror-like.
+         
+         We need to rotate the image 90º clockwise, and flip it.
+         
+         The comment below each transform operation shows the transformation state after said operation.
+         w = the original width of the image
+         h = the original height of the image
+         */
+        
+        var transform = CGAffineTransform.identity
+        
+        /*
+          _____ (w,h)
+         |     |
+         |_____|
+         (0,0)
+         
+         */
+        
+        transform = transform.translatedBy(x: image.extent.height, y: image.extent.width)
+        
+        /*
+          _____ (h+w,w+h)
+         |     |
+         |_____|
+         (h,w)
+         x
+         (0,0)
+         */
+        
+        transform = transform.rotated(by: -CGFloat(M_PI_2))
+        
+        /*
+          ____(h+h,w)
+         |    |
+         |    |
+         |____|
+         (h,0)
+         */
+        
+        //We're going to mirror the image so it looks like a ...mirror
+        transform = transform.scaledBy(x: 1, y: -1)
+        
+        /*
+         ____ (h,w)
+         |    |
+         |    |
+         |____|
+         (0,0)
+         */
+        
+        transformFilter.setValue(NSValue(cgAffineTransform: transform), forKey: "inputTransform")
+        
+        //first, rotate the image because it comes in landscape
+        transformFilter.setValue(image, forKey: "inputImage")
+        
+        let rotated = transformFilter.value(forKey: "outputImage") as! CIImage
+        
+        let cgImage = context.createCGImage(rotated, from: rotated.extent)
+        
+        view.layer.contents = cgImage
+        
+        //find faces in the image
+        let features = detector.features(in: rotated)
+        
+        let uiTransform = CGAffineTransform(translationX: 0, y: view.layer.frame.height).scaledBy(x: view.layer.frame.width/rotated.extent.width, y: -view.layer.frame.height/rotated.extent.height)
+        
+        //only handle one feature
+        if features.count > 0 {
+            let faceFeature = features[0] as! CIFaceFeature
+            
+            let faceRect = faceFeature.bounds.applying(uiTransform)
+            
+            faceLayer.opacity = 1
+            
+            faceLayer.frame = faceRect
+            
+            if faceFeature.hasFaceAngle {
+                
+                let rotateRadians = (CGFloat(faceFeature.faceAngle) / 360) * (2 * CGFloat(M_PI))
+                
+                faceLayer.transform = CATransform3DMakeRotation(rotateRadians, 0, 1, 1)
+            } else {
+                faceLayer.transform = CATransform3DIdentity
+            }
+            
+            //if we have the mouth
+            if faceFeature.hasMouthPosition {
+                mouthLayer.opacity = 1
+                
+                //set the mouth's position as the mouth position translated to ui space then translated into facelayer space
+                
+                let position = view.layer.convert(faceFeature.mouthPosition.applying(uiTransform), to: faceLayer)
+                
+                var frame = faceLayer.frame
+                
+                frame.size.width *= 0.5
+                frame.size.height *= 0.2
+                
+                mouthLayer.frame = frame
+                
+                mouthLayer.position = position
+                
+                
+            } else {
+                mouthLayer.opacity = 0
+            }
+            
+            //if we have the left eye
+            if faceFeature.hasLeftEyePosition {
+                leftEyeLayer.opacity = 1
+                
+                let position = view.layer.convert(faceFeature.leftEyePosition.applying(uiTransform), to: faceLayer)
+                
+                var frame = faceLayer.frame
+                
+                frame.size.width *= 0.2
+                frame.size.height *= 0.2
+                
+                leftEyeLayer.frame = frame
+                
+                leftEyeLayer.position = position
+            } else {
+                leftEyeLayer.opacity = 0
+            }
+
+            
+        } else {
+            faceLayer.opacity = 0
+        }
     }
 
 }
